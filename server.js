8f7fa1c72f42b4ea3f8d99fbf96a3c83076e74f2
muneeb.rehman <muneeb.rehman@formulatrix.com>
2023-08-04 16:45:09 +0500

    saved

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('server is working ');
+import express from 'express';
+const app = express();
+
+import morgan from 'morgan';
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({ message: 'data received', data: req.body });
+});
+
+app.listen(5100, () => {
+  console.log('server running...');
+});
