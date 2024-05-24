092f019e736c65a6a26f7f675b5d17193e36ca49
muneeb.rehman <muneeb.rehman@formulatrix.com>
2024-05-24 15:21:06 +0500

    express validator

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.post('/', (req, res) => {
-  console.log(req);
-  res.json({ message: 'data received', data: req.body });
+app.get('/api/v1/test', (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `data received name = ${name}` });
 });
 
 app.use('/api/v1/jobs', jobRouter);
