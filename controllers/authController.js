1b0b37b60dd2bfd64fe8392504889b7b762d3361
muneeb.rehman <muneeb.rehman@formulatrix.com>
2024-05-30 16:06:30 +0500

    JWT added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -15,10 +16,17 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
-
   const isValid =
     user && (await comparePassword(req.body.password, user.password));
-
   if (!isValid) throw new UnauthenticatedError('invalid credentials');
-  res.send('login route');
+  const token = createJWT({ userId: user._id, role: user.role });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+  res.json({ token: token });
 };
